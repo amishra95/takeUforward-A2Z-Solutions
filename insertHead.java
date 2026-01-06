5ccfde4f514c241351565abbffbdff8b7c76eb7c
Avanish Mishra <avanish.neham@gmail.com>
2026-01-06 19:37:27 +0530

    Create insertHead.java

---
+++ insertHead.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode insertAtHead(ListNode head, int X) {
+        
+        ListNode newnode = new ListNode(X);
+
+        newnode.next = head;
+        head = newnode;
+
+        return head;
+        
+    }
+}
