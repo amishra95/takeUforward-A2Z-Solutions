fd9f439d858a444f95057182ceb21e2e0adcae03
Avanish Mishra <avanish.neham@gmail.com>
2026-01-17 21:50:23 +0530

    Update singleNumber1.java

--- BitManipulation/singleNumber1.java
+++ BitManipulation/singleNumber1.java
@@ -12,3 +12,16 @@ class Solution {
         return nums[nums.length-1];
     }
 }
+// 2nd Solution using Single Number and Xor
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+    for(int i = 0; i < nums.length; i++){
+        a ^= nums[i];
+    }
+
+        return a;
+
+    }
+}
+
