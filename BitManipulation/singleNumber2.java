0ef52aa123014bbeca00d2b1daa0ea3861eeeb26
Avanish Mishra <avanish.neham@gmail.com>
2026-01-17 21:51:08 +0530

    Create singleNumber2.java

---
+++ BitManipulation/singleNumber2.java
@@ -0,0 +1,17 @@
+// Solution using Sorting Method. 
+class Solution {
+    public int singleNumber(int[] nums) {
+       
+
+    Arrays.sort(nums);
+
+    for(int i = 0 ; i < nums.length-1; i+=3){
+        if(nums[i] != nums[i+2]){
+            return nums[i];
+        }
+    }    
+    return nums[nums.length-1];
+         
+         }
+
+}
