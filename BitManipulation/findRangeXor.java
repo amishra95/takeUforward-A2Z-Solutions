cae6274b6f931505c5e9dc02bde49d8b00ec2f58
Avanish Mishra <avanish.neham@gmail.com>
2026-01-19 22:16:39 +0530

    Update findRangeXor.java

--- BitManipulation/findRangeXor.java
+++ BitManipulation/findRangeXor.java
@@ -9,3 +9,17 @@ class Solution {
         return solve;
     }
 }
+// Faster and O(1) method
+class Solution {
+    public int findRangeXOR(int l, int r) {
+
+        return XORend(l-1) ^ XORend(r);
+    }
+
+    private int XORend(int n){
+        if(n % 4 == 1) return 1;
+        if(n% 4 == 2) return n+1;
+        if(n % 4 == 3) return 0;
+        return n;
+    }
+}
