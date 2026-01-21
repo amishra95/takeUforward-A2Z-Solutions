a92b57887bd7fbf49b5d1f4d1f00b93fe3947241
Avanish Mishra <avanish.neham@gmail.com>
2026-01-21 16:24:39 +0530

    Create lemonadeChange.java

---
+++ greedyAlgo/lemonadeChange.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int countFive = 0;
+        int countTen = 0;
+      
+
+    for(int i = 0; i < bills.length; i++){
+        if(bills[i] == 5){
+            countFive++;
+        }
+        else if(bills[i] == 10){
+            if(countFive == 0){
+                return false;
+            }
+            countFive--;
+            countTen++;
+        }
+        else {
+            if(countTen > 0 && countFive > 0){
+                countTen--;
+                countFive--;
+            }
+            else if(countFive  >= 3){
+                countFive -=3;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+        return true;
+
+
+
+    }
+}
