3bd3b1856e428d0348238a8de394d45a25ab84aa
Avanish Mishra <avanish.neham@gmail.com>
2026-02-09 13:21:47 +0530

    Update nextLargeElement.java

--- Stack/nextLargeElement.java
+++ Stack/nextLargeElement.java
@@ -1,3 +1,28 @@
+//Tried the Monotonic Stack case
+
+class Solution {
+    public int[] nextLargerElement(int[] arr) {
+
+        Stack<Integer> stack = new Stack<>();
+        int[] a = new int[arr.length];
+
+    for(int i = arr.length-1; i >= 0; i--){
+        while(!stack.isEmpty() && stack.peek() <= arr[i]){
+            stack.pop();
+        }
+    
+        if(stack.isEmpty()){
+            a[i] = -1;
+        }
+        else
+            a[i] = stack.peek();
+            stack.push(arr[i]);
+
+    }
+    
+    return a;
+    }
+}
 
 // Improper Solution and not the defined result value. (Need to use Stack)
 class Solution {
