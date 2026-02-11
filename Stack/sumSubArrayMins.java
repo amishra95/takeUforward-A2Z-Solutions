e802b9945727263266ac7cc42a08ed183527fb36
Avanish Mishra <avanish.neham@gmail.com>
2026-02-11 10:57:13 +0530

    Update sumSubArrayMins.java

--- Stack/sumSubArrayMins.java
+++ Stack/sumSubArrayMins.java
@@ -1,4 +1,69 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int MOD = (int)1e9 + 7;
+        
+        // Get the index of previous smaller and next smaller elements
+        int[] prevSmaller = prevSmallestElemnt(arr);
+        int[] nextSmaller = nextSmallestElement(arr);
+        
+        long ans = 0;
+        
+        // Calculate contribution of each element
+        for (int i = 0; i < n; i++) {
+            // Calculate how many positions left we can extend
+            int left = i - prevSmaller[i];  // distance to previous smaller
+            
+            // Calculate how many positions right we can extend
+            int right = nextSmaller[i] - i;  // distance to next smaller
+            
+            // Number of subarrays where arr[i] is minimum = left * right
+            // Contribution = arr[i] * left * right
+            long contribution = (long) arr[i] * left * right;
+            
+            ans = (ans + contribution) % MOD;
+        }
+        
+        return (int) ans;
+    }
 
+    private int[] nextSmallestElement(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = n-1; i >= 0; i--){
+            int currEle = arr[i];
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
+                stack.pop();
+            }
+
+            ans[i] = !stack.isEmpty() ? stack.peek() : n;
+            stack.push(i);
+        }
+    
+        return ans;
+    }
+
+    private int[] prevSmallestElemnt(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+            int currEle = arr[i];
+
+            while(!stack.isEmpty() && arr[stack.peek()] > arr[i]){
+                stack.pop();
+            }
+            ans[i] = !stack.isEmpty() ? stack.peek() : -1;
+            stack.push(i);
+        }
+        return ans;
+    }
+}
 
 
 
