0437b4433185c55a0dae7b341da480106b7b1005
Avanish Mishra <avanish.neham@gmail.com>
2026-02-11 09:53:17 +0530

    Create sumSubArrayMins.java

---
+++ Stack/sumSubArrayMins.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        long sum = 0;
+        
+        int mod = (int)1e9 + 7;
+    
+// Find the subarray Minimums and solution base involved in them?
+
+    for(int i = 0; i < arr.length; i++){
+       int mini = arr[i];
+
+    for(int j = i; j < arr.length; j++){
+        mini = Math.min(mini,  arr[j]);
+        sum = (sum + mini) % mod;
+
+    }
+
+    }
+
+
+    return (int) sum;
+
+    }
+}
