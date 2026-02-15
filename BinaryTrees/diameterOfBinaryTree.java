2c0c5c416d329ba2a03e923e7f09fee9b5164761
Avanish Mishra <avanish.neham@gmail.com>
2026-02-15 18:10:56 +0530

    Create diameterOfBinaryTree.java

---
+++ BinaryTrees/diameterOfBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int data;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int val) { data = val; left = null, right = null }
+ * }
+ **/
+
+class Solution {
+   int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+            maxDepth(root);
+        
+            return max;
+    }
+
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+         max = Math.max(max,left+right);
+        
+        return Math.max(left, right) + 1;
+
+
+
+    }
+}
