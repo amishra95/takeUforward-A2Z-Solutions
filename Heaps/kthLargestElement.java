756a2d35738c52bb0613d09e1ab039889cf5428d
Avanish Mishra <avanish.neham@gmail.com>
2026-03-03 16:04:02 +0530

    Update kthLargestElement.java

--- Heaps/kthLargestElement.java
+++ Heaps/kthLargestElement.java
@@ -1,4 +1,19 @@
+// Use of Min-Heap as a model to solve the question
+class Solution {
+    public int kthLargestElement(int[] nums, int k) {
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 
+        for(int num: nums){
+            queue.offer(num);
+            if(queue.size() > k){
+                queue.poll();
+            }
+        }
+
+        return queue.peek();
+    }
+}
 
 // Solved it using Arrays Sort. 
 class Solution {
