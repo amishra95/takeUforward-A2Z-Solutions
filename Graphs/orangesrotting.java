730ec36525cda1dcb7e8e6bcbbb4ac9872dfc92e
Avanish Mishra <avanish.neham@gmail.com>
2026-03-08 17:38:27 +0530

    Create orangesrotting.java

---
+++ Graphs/orangesrotting.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+     
+    }
+    
+    private boolean isValid(int i, int j, int n, int m){
+        if(i < 0 || i >= n) return false;
+        if(j < 0 || j >= m)  return false;
+    
+        return true;
+
+    }
+
+}
+
+
