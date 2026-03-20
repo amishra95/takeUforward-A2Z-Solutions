db50fdcd27c49c8197d7406134cc0c054688b1e7
Avanish Mishra <avanish.neham@gmail.com>
2026-03-20 08:57:45 +0530

    Create floydWarshall.java

---
+++ Graphs/floydWarshall.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void shortestDistance(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int k =0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(matrix[i][k] == -1 || matrix[k][j] == -1){
+                    continue;
+                    }
+                    if(matrix[i][j] == -1){
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
+                    else{
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }
+  
+    }
+}
+
