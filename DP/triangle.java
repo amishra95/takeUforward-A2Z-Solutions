b36dbc535ad7e08e2e5ad701d0f49c14295638bc
Avanish Mishra <avanish.neham@gmail.com>
2026-03-30 10:19:45 +0530

    Create triangle.java

---
+++ DP/triangle.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minTriangleSum(int[][] triangle) {
+
+        int n = triangle.length;
+        int[] dp = new int[n];
+    
+        
+    for(int j = 0; j < n; j++){
+        dp[j] = triangle[n-1][j];
+        }
+
+    for(int i = n-2; i >=0; i--){
+        for(int j = 0; j <= i; j++){
+        dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);  
+        }
+    }
+    
+    return dp[0];
+
+        
+    }
+}
