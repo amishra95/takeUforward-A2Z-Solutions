9f8f639ecf793c6669af0314735ea18b3e629ba8
Avanish Mishra <avanish.neham@gmail.com>
2026-04-11 08:54:41 +0530

    Create longestCommonSubseq.java

---
+++ DP/longestCommonSubseq.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int lcs(String str1, String str2) {
+    if (str1 == null || str1.length() == 0) {
+      return 0;
+    }
+    if (str2 == null || str2.length() == 0) {
+      return 0;
+    }
+    
+    int n = str1.length();
+    int m = str2.length();
+
+    int dp[][] = new int[n+1][m+1];
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        if(str1.charAt(i-1) == str2.charAt(j-1)){
+            dp[i][j] = dp[i-1][j-1] + 1;
+        }
+        else{
+            dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+        }
+      }
+    }
+    return dp[n][m];
+  }
+}
