37c85e796d3e5cafec1b881439d587492486166b
Avanish Mishra <avanish.neham@gmail.com>
2026-04-12 09:26:17 +0530

    Create wildCardMatching.java

---
+++ DP/wildCardMatching.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean wildCard(String str, String pat) {
+        if(str == null || pat == null) return false;  
+
+        int n = str.length();
+        int m = pat.length();
+
+        boolean dp[][] = new boolean[n+1][m+1];
+        
+        dp[0][0] = true;
+
+        for(int j = 1; j <= m; j++){
+            if(pat.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <=m; j++){
+                if(str.charAt(i-1) == pat.charAt(j-1) || pat.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(pat.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+            return dp[n][m];
+
+    }
+
+}
