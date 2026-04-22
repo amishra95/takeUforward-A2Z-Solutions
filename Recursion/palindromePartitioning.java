0343251a44c214f20aeb536cb25eeed9afa4790e
Avanish Mishra <avanish.neham@gmail.com>
2026-04-22 08:59:09 +0530

    Create palindromePartitioning.java

---
+++ Recursion/palindromePartitioning.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+
+           
+    }
+
+    public void backtrack(String s, int index, List<String> current, List<List<String>> result){
+        if(index == s.length()){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for(int i = index; i < s.length(); i++){
+            if(isPalindrome(s, index, i)){
+                current.add(s.substring(index, i+1));
+                backtrack(s, i+1, current, result);
+                current.remove(current.size()-1);
+            }
+
+        }
+    }
+
+    public boolean isPalindrome(String s, int left, int right){
+           while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+           }
+        return true;
+    }
+}
