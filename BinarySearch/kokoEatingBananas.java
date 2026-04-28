cce1b5f717c4f899f148af0c838042d11d746bcf
Avanish Mishra <avanish.neham@gmail.com>
2026-04-28 11:28:17 +0530

    Update kokoEatingBananas.java

--- BinarySearch/kokoEatingBananas.java
+++ BinarySearch/kokoEatingBananas.java
@@ -1,3 +1,25 @@
+class Solution {
+    public int minimumRateToEatBananas(int[] nums, int h) {
+        int lo = 1;
+        int high = Arrays.stream(nums).max().getAsInt();
+
+        while(lo < high){
+            int mid = lo +(high-lo)/2;
+            int sum = 0;
+            for(int i = 0; i < nums.length; i++){
+                sum += (nums[i]+mid-1)/mid;
+            }
+            if(sum <= h){
+                high = mid;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
+        return lo;
+    }
+}
+
 //TLE error, needs binary Search. 
 class Solution {
     public int minimumRateToEatBananas(int[] nums, int h) {
