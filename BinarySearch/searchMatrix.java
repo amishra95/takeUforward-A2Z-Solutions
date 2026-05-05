5a04f7db799cef536bb69a92c7ad1de42ccce380
Avanish Mishra <avanish.neham@gmail.com>
2026-05-05 12:58:58 +0530

    Update searchMatrix.java

--- BinarySearch/searchMatrix.java
+++ BinarySearch/searchMatrix.java
@@ -1,3 +1,35 @@
+//Fastest solution O(Log(n*m))
+class Solution {
+    public boolean searchMatrix(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+    int left = 0;
+    int right = n*m-1;
+
+    while(left <= right){
+        int mid = left + (right-left)/2;
+        int row = mid/m;
+        int col = mid%m;
+
+        if(mat[row][col]== target){
+            return true;
+        }
+        else if(mat[row][col] < target){
+            left = mid+1;
+
+        }
+        else{
+
+        right = mid-1;
+        }
+
+}
+    return false;
+}
+    
+}
+
 //Worst Solution
 class Solution {
     public boolean searchMatrix(int[][] mat, int target) {
