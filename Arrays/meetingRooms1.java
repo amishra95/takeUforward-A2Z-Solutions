eea0fc621a66cfad8832c15cfd785388a34dccb3
Avanish Mishra <avanish.neham@gmail.com>
2026-05-11 09:20:27 +0530

    Create meetingRooms1.java

---
+++ Arrays/meetingRooms1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i-1][1] > intervals[i][0]){
+                return false;
+            }
+        }
+
+    return true;
+
+       
+    }
+}
+
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int n = intervals.length;
+          
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(intervals[i][1] > intervals[j][0] && intervals[i][0] < intervals[j][1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+       
+    }
+}
