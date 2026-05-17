0141a5b83f9afb339fa9af2b5c693c8ffcebebde
Avanish Mishra <avanish.neham@gmail.com>
2026-05-17 09:15:06 +0530

    Update maxSlidingWindow.java

--- Stack/maxSlidingWindow.java
+++ Stack/maxSlidingWindow.java
@@ -1,3 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        int n = arr.length;
+        int[] result = new int[n - k + 1];
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> b[0] - a[0]
+        );
+
+        for (int i = 0; i < n; i++) {
+
+            maxHeap.offer(new int[]{arr[i], i});
+
+            while (maxHeap.peek()[1] < i - k + 1) {
+                maxHeap.poll();
+            }
+
+            if (i >= k - 1) {
+                result[i - k + 1] = maxHeap.peek()[0];
+            }
+        }
+
+        return result;
+    }
+}
+
 //TLE ( needs improvement from the Brute Force structure)
 class Solution {
   public int[] maxSlidingWindow(int[] arr, int k) {
