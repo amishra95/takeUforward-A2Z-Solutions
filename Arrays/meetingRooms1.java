bf5cb66a4eb6f27daaaa798a6170b11e49d66f29
Avanish Mishra <avanish.neham@gmail.com>
2026-05-21 18:06:57 +0530

    Update meetingRooms1.java

--- Arrays/meetingRooms1.java
+++ Arrays/meetingRooms1.java
@@ -3,7 +3,6 @@ class Solution {
         
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         
-
         for(int i = 1; i < intervals.length; i++){
             if(intervals[i-1][1] > intervals[i][0]){
                 return false;
