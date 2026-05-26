1dfd53c3a0638def86d89805cc1ab2aca28cec66
Avanish Mishra <avanish.neham@gmail.com>
2026-05-26 11:10:44 +0530

    Update longestConsecutive.java

--- HashMap/longestConsecutive.java
+++ HashMap/longestConsecutive.java
@@ -1,3 +1,27 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        int longest = 0;
+
+        for(int a:set){
+            if(!set.contains(a-1)){
+                int current = 1;
+                while(set.contains(a + current)){
+                    current++;
+                }
+         longest = Math.max(longest, current);
+
+            }
+        }
+        return longest;
+    }
+}
+
 // Use of Sorting not Hash Map
 class Solution {
     public int longestConsecutive(int[] nums) {
