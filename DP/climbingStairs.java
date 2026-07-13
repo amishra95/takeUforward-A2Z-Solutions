235512e3e48b36bb02524aa46edaa07ac45e0baf
Avanish Mishra <avanish.neham@gmail.com>
2026-07-13 18:07:46 +0530

    Update climbingStairs.java

--- DP/climbingStairs.java
+++ DP/climbingStairs.java
@@ -14,3 +14,27 @@ class Solution {
     return dp[n];
     }
 }
+
+// Memoization approach below
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        return dp[n] = func(n, dp);
+
+    }
+
+    public int func(int n, int[] dp){
+        if(n <= 1){
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n]
+ =func(n-1, dp) + func(n-2, dp);
+     }
+}
