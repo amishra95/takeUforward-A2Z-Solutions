a8623cbcf662b8869b56c3aac9a3191689f4f570
Avanish Mishra <avanish.neham@gmail.com>
2026-07-29 14:23:04 +0530

    Update moveZeroesToEnd.java

--- Arrays/moveZeroesToEnd.java
+++ Arrays/moveZeroesToEnd.java
@@ -1,3 +1,20 @@
+// Another way to solve this
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+    for(int i = 0; i < nums.length; i++){
+        if(nums[i] != 0){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+        while(j < nums.length){
+            nums[j++] = 0;
+         }
+    
+    }
+}
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int index = 0;
