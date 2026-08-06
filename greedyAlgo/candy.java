780221203f129ba78224a4e906a406495a3a0033
Avanish Mishra <avanish.neham@gmail.com>
2026-08-06 18:13:55 +0530

    Update candy.java

--- greedyAlgo/candy.java
+++ greedyAlgo/candy.java
@@ -1,3 +1,39 @@
+//@newsolution 
+class Solution {
+    public int candy(int[] ratings) {
+    
+    int n = ratings.length;
+    int count = 0;
+
+    int[] arr = new int[n];
+
+    Arrays.fill(arr, 1);
+
+    for(int i = 1; i < n; i++){
+       if(ratings[i] > ratings[i-1]){
+            arr[i] = arr[i-1] +1;
+       }
+    }
+    for(int i = n-2; i >= 0; i--){
+        if(ratings[i] > ratings[i+1]){
+            arr[i] = Math.max(arr[i], arr[i+1]+1);
+        }
+    }
+
+    for(int b : arr){
+        count += b;
+    }
+   
+
+
+return count;
+
+        
+
+    }
+}
+
+
 class Solution {
     public int candy(int[] ratings) {
        //dynamic programming case and needed condition
